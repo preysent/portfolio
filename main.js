8fd6994641ac1cf19b40de3011928cc9dbc66084
preysent <preysent@gmail.com>
2024-02-02 22:51:29 +0530

    basic page ready

--- main.js
+++ main.js
@@ -6,7 +6,19 @@ const scroll = new LocomotiveScroll({
 });
 
 const handleNav =(e)=>{
+    console.log("clicked ")
     const el = document.getElementById((e.target.innerText.toLowerCase()))
     scroll.scrollTo(el)
 }
 document.querySelector('nav').addEventListener('click',handleNav)
+
+const ob = document.querySelector('#hero-obj')
+
+// window.addEventListener("mousemove",(e)=>{
+//     const x = e.clientX/1- window.innerWidth/2
+//     const y = e.clientY/window.innerHeight*1.5 
+
+//     ob.style.transform=`translate3d(${-x}px,0,0) rotate(15deg) scale(${y})`
+//     console.log(y)
+// })
+
